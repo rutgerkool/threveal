98f5c554d7da64640e44fe136c19c76b6ed8b531
Rutger Kool <rutgerkool@gmail.com>
2026-01-25 22:20:46 +0100

    Add eBPF library target and update FindLibBpf for libbpf 1.3.0

--- cmake/FindLibBpf.cmake
+++ cmake/FindLibBpf.cmake
@@ -7,47 +7,58 @@
 #   LIBBPF_INCLUDE_DIRS - Include directories for libbpf
 #   LIBBPF_LIBRARIES    - Libraries to link against
 #   LibBpf::LibBpf      - Imported target for libbpf
-#
-# The following variables can be set as hints:
-#   LIBBPF_ROOT_DIR     - Root directory to search for libbpf
 
 include(FindPackageHandleStandardArgs)
 
-# Try pkg-config first (preferred method)
-find_package(PkgConfig QUIET)
-if(PKG_CONFIG_FOUND)
-    pkg_check_modules(PC_LIBBPF QUIET libbpf)
-endif()
-
-# Find the include directory
-find_path(LIBBPF_INCLUDE_DIR
-    NAMES bpf/libbpf.h
-    HINTS
-        ${PC_LIBBPF_INCLUDEDIR}
-        ${PC_LIBBPF_INCLUDE_DIRS}
-        ${LIBBPF_ROOT_DIR}/include
-    PATHS
-        /usr/include
-        /usr/local/include
-)
-
-# Find the library
+# Find the library first (prefer /usr/lib64 for manually installed newer versions)
+# This determines which version we'll use before checking pkg-config
 find_library(LIBBPF_LIBRARY
     NAMES bpf
-    HINTS
-        ${PC_LIBBPF_LIBDIR}
-        ${PC_LIBBPF_LIBRARY_DIRS}
-        ${LIBBPF_ROOT_DIR}/lib
     PATHS
-        /usr/lib
-        /usr/lib/x86_64-linux-gnu
+        /usr/lib64
+        /usr/local/lib64
         /usr/local/lib
+    NO_DEFAULT_PATH
 )
+
+# Fallback to system paths if not found in preferred locations
+if(NOT LIBBPF_LIBRARY)
+    find_library(LIBBPF_LIBRARY NAMES bpf)
+endif()
+
+# Find the include directory based on where we found the library
+if(LIBBPF_LIBRARY MATCHES "^/usr/lib64")
+    # Use headers from manually installed version
+    find_path(LIBBPF_INCLUDE_DIR
+        NAMES bpf/libbpf.h
+        PATHS /usr/local/include /usr/include
+        NO_DEFAULT_PATH
+    )
+    set(LIBBPF_VERSION "1.3.0")
+else()
+    # Use system headers
+    find_path(LIBBPF_INCLUDE_DIR
+        NAMES bpf/libbpf.h
+        PATHS /usr/include
+    )
+    # Try to get version from pkg-config
+    find_package(PkgConfig QUIET)
+    if(PKG_CONFIG_FOUND)
+        pkg_check_modules(PC_LIBBPF QUIET libbpf)
+        if(PC_LIBBPF_VERSION)
+            set(LIBBPF_VERSION ${PC_LIBBPF_VERSION})
+        else()
+            set(LIBBPF_VERSION "unknown")
+        endif()
+    else()
+        set(LIBBPF_VERSION "unknown")
+    endif()
+endif()
 
 # Handle the REQUIRED/QUIET arguments and set LIBBPF_FOUND
 find_package_handle_standard_args(LibBpf
     REQUIRED_VARS LIBBPF_LIBRARY LIBBPF_INCLUDE_DIR
-    VERSION_VAR PC_LIBBPF_VERSION
+    VERSION_VAR LIBBPF_VERSION
 )
 
 # Set output variables
